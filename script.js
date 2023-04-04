446e27feb7f1f44b56ac71043f8c632a1f4490bd
rumi0811 <sekarpuspitaarum08@gmail.com>
2023-04-04 20:25:37 +0700

    Argh the JavaScript not yet

---
+++ script.js
@@ -0,0 +1,7 @@
+const logreBox = document.querySelector('.logreg-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', () => {
+    logreBox.classList.add('active');
+});
